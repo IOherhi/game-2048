f3d211138723de037d2ef3d2726f7486f70e49b0
Ivan Herhi <ionherhi@gmail.com>
2025-07-01 16:35:43 +0200

    update01

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /* ========================================== */
 
-const tbody = document.querySelector('tbody');
-const buttonOfStart = document.querySelector('button');
+const tbody = document.querySelector("tbody");
+const buttonOfStart = document.querySelector("button");
 const arrayTr = Array.from(tbody.children);
 
 /* ===================================================== */
@@ -12,7 +12,7 @@ class Game {
   constructor(ss) {
     this.board = this.createEmptyBoard();
     this.score = 0;
-    this.status = 'playing'; // або 'finished' (гра завершена)
+    this.status = "playing"; // або 'finished' (гра завершена)
   }
 
   start() {
@@ -27,14 +27,14 @@ class Game {
   }
 
   deleteMessage() {
-    const message = document.querySelector('.message-container');
+    const message = document.querySelector(".message-container");
     const messageStart = message.children[2];
 
-    messageStart.classList.remove('message-lose');
-    messageStart.classList.remove('message-win');
-    messageStart.classList.remove('message');
+    messageStart.classList.remove("message-lose");
+    messageStart.classList.remove("message-win");
+    messageStart.classList.remove("message");
 
-    messageStart.textContent = '';
+    messageStart.textContent = "";
   }
 
   spawnTile() {
@@ -44,7 +44,7 @@ class Game {
     let canAddNewElement = false;
 
     for (const array of arraytr) {
-      if (Array.from(array.children).some((td) => td.textContent === '')) {
+      if (Array.from(array.children).some((td) => td.textContent === "")) {
         canAddNewElement = true;
         break; // Зупиняємо цикл, якщо знайшли порожню клітинку
       }
@@ -57,16 +57,16 @@ class Game {
       while (!finish) {
         randomTd = arrayTr[randomNumber()].children[randomNumber()];
 
-        if (!randomTd.classList.contains('activ')) {
+        if (!randomTd.classList.contains("activ")) {
           randomTd.textContent = randomTwoOrForth();
-          randomTd.classList.add('activ');
+          randomTd.classList.add("activ");
 
           if (+randomTd.textContent === 2) {
-            randomTd.classList.add('field-cell--2');
+            randomTd.classList.add("field-cell--2");
           }
 
           if (+randomTd.textContent === 4) {
-            randomTd.classList.add('field-cell--4');
+            randomTd.classList.add("field-cell--4");
           }
 
           finish = true; // Зупиняємо цикл, коли плитка додана
@@ -79,44 +79,46 @@ class Game {
     Array.from(tbody.children).forEach((tr, indexTr) => {
       Array.from(tr.children).forEach((td, indexTd) => {
         this.board[indexTr][indexTd] =
-          td.textContent === '' ? 0 : td.textContent;
+          td.textContent === "" ? 0 : td.textContent;
       });
     });
   }; // Перезапускає поле.
 
   clearBoard() {
-    const cells = tbody.querySelectorAll('td');
+    const cells = tbody.querySelectorAll("td");
 
     cells.forEach((cell) => {
-      cell.textContent = '';
-      cell.className = 'field-cell';
-      cell.removeAttribute('style');
+      cell.textContent = "";
+      cell.className = "field-cell";
+      cell.removeAttribute("style");
     });
   } // Очищає поле гри.
 
   restart() {
     this.board = this.createEmptyBoard();
     this.score = 0;
-    this.status = 'playing';
+    this.status = "playing";
 
-    const button = document.querySelector('.button');
-    const messageConT = document.querySelector('.message-container');
+    const button = document.querySelector(".button");
+    const messageConT = document.querySelector(".message-container");
 
-    if (button.textContent === 'Start') {
-      button.style.fontSize = '18px';
-      button.textContent = 'Restart';
-      button.classList.add('restart');
+    if (button.textContent === "Start") {
+      button.style.fontSize = "18px";
+      button.textContent = "Restart";
+      button.classList.add("restart");
     } else {
-      button.style.fontSize = '18px';
-      button.textContent = 'Start';
-      button.classList.remove('restart');
-      button.classList.add('start');
+      button.style.fontSize = "18px";
+      button.textContent = "Start";
+      button.classList.remove("restart");
+      button.classList.add("start");
 
       messageConT.children[2].textContent =
         'Press "Start" to begin game. Good luck!';
-      messageConT.children[2].classList.add('message-start');
-      messageConT.children[2].classList.add('message');
+      messageConT.children[2].classList.add("message-start");
+      messageConT.children[2].classList.add("message");
     }
+
+    this.deleteMessage();
   } // Перезапускає гру, обнуляючи рахунки.
 
   createEmptyBoard() {
@@ -143,47 +145,47 @@ class Game {
 
         switch (+tdContent) {
           case 2:
-            td.classList.add('field-cell--2');
+            td.classList.add("field-cell--2");
             break;
 
           case 4:
-            td.classList.add('field-cell--4');
+            td.classList.add("field-cell--4");
             break;
 
           case 8:
-            td.classList.add('field-cell--8');
+            td.classList.add("field-cell--8");
             break;
 
           case 16:
-            td.classList.add('field-cell--16');
+            td.classList.add("field-cell--16");
             break;
 
           case 32:
-            td.classList.add('field-cell--32');
+            td.classList.add("field-cell--32");
             break;
 
           case 64:
-            td.classList.add('field-cell--64');
+            td.classList.add("field-cell--64");
             break;
 
           case 128:
-            td.classList.add('field-cell--128');
+            td.classList.add("field-cell--128");
             break;
 
           case 256:
-            td.classList.add('field-cell--256');
+            td.classList.add("field-cell--256");
             break;
 
           case 512:
-            td.classList.add('field-cell--512');
+            td.classList.add("field-cell--512");
             break;
 
           case 1024:
-            td.classList.add('field-cell--1024');
+            td.classList.add("field-cell--1024");
             break;
 
           case 2048:
-            td.classList.add('field-cell--2048');
+            td.classList.add("field-cell--2048");
             break;
         }
       });
@@ -268,32 +270,32 @@ class Game {
   }
 
   addSCore() {
-    const gameScore = document.querySelector('.game-score');
+    const gameScore = document.querySelector(".game-score");
 
     gameScore.textContent = this.score;
   }
 
   getMessage() {
-    const messageConT = document.querySelector('.message-container');
+    const messageConT = document.querySelector(".message-container");
 
     messageConT.children[2].textContent = messageConT.children[0].textContent;
-    messageConT.children[2].classList.add('message-lose');
-    messageConT.children[2].classList.add('message');
+    messageConT.children[2].classList.add("message-lose");
+    messageConT.children[2].classList.add("message");
   }
 
   getMessageWin() {
-    const messageConT = document.querySelector('.message-container');
-    const messageWin = document.querySelector('.message-win');
+    const messageConT = document.querySelector(".message-container");
+    const messageWin = document.querySelector(".message-win");
 
     messageConT.children[2].textContent = messageWin.textContent;
-    messageConT.children[2].classList.add('message-win');
-    messageConT.children[2].classList.add('message');
+    messageConT.children[2].classList.add("message-win");
+    messageConT.children[2].classList.add("message");
   }
 
   moveUp() {
     if (!this.canContinueGame()) {
       this.getMessage();
-      this.status = 'finished';
+      this.status = "finished";
 
       return;
     }
@@ -354,25 +356,25 @@ class Game {
     for (let i = 0; i < arrayFromTr.length; i++) {
       const currentTr = arrayFromTr[i].children;
 
-      currentTr[0].textContent = newColumn1[i] === 0 ? '' : newColumn1[i];
-      currentTr[1].textContent = newColumn2[i] === 0 ? '' : newColumn2[i];
-      currentTr[2].textContent = newColumn3[i] === 0 ? '' : newColumn3[i];
-      currentTr[3].textContent = newColumn4[i] === 0 ? '' : newColumn4[i];
+      currentTr[0].textContent = newColumn1[i] === 0 ? "" : newColumn1[i];
+      currentTr[1].textContent = newColumn2[i] === 0 ? "" : newColumn2[i];
+      currentTr[2].textContent = newColumn3[i] === 0 ? "" : newColumn3[i];
+      currentTr[3].textContent = newColumn4[i] === 0 ? "" : newColumn4[i];
 
-      if (currentTr[0].textContent !== '') {
-        currentTr[0].classList.add('activ');
+      if (currentTr[0].textContent !== "") {
+        currentTr[0].classList.add("activ");
       }
 
-      if (currentTr[1].textContent !== '') {
-        currentTr[1].classList.add('activ');
+      if (currentTr[1].textContent !== "") {
+        currentTr[1].classList.add("activ");
       }
 
-      if (currentTr[2].textContent !== '') {
-        currentTr[2].classList.add('activ');
+      if (currentTr[2].textContent !== "") {
+        currentTr[2].classList.add("activ");
       }
 
-      if (currentTr[3].textContent !== '') {
-        currentTr[3].classList.add('activ');
+      if (currentTr[3].textContent !== "") {
+        currentTr[3].classList.add("activ");
       }
     }
 
@@ -458,25 +460,25 @@ class Game {
     for (let i = 0; i < arrayFromTr.length; i++) {
       const currentTr = arrayFromTr[i].children;
 
-      currentTr[0].textContent = newColumn1[i] === 0 ? '' : newColumn1[i];
-      currentTr[1].textContent = newColumn2[i] === 0 ? '' : newColumn2[i];
-      currentTr[2].textContent = newColumn3[i] === 0 ? '' : newColumn3[i];
-      currentTr[3].textContent = newColumn4[i] === 0 ? '' : newColumn4[i];
+      currentTr[0].textContent = newColumn1[i] === 0 ? "" : newColumn1[i];
+      currentTr[1].textContent = newColumn2[i] === 0 ? "" : newColumn2[i];
+      currentTr[2].textContent = newColumn3[i] === 0 ? "" : newColumn3[i];
+      currentTr[3].textContent = newColumn4[i] === 0 ? "" : newColumn4[i];
 
-      if (currentTr[0].textContent !== '') {
-        currentTr[0].classList.add('activ');
+      if (currentTr[0].textContent !== "") {
+        currentTr[0].classList.add("activ");
       }
 
-      if (currentTr[1].textContent !== '') {
-        currentTr[1].classList.add('activ');
+      if (currentTr[1].textContent !== "") {
+        currentTr[1].classList.add("activ");
       }
 
-      if (currentTr[2].textContent !== '') {
-        currentTr[2].classList.add('activ');
+      if (currentTr[2].textContent !== "") {
+        currentTr[2].classList.add("activ");
       }
 
-      if (currentTr[3].textContent !== '') {
-        currentTr[3].classList.add('activ');
+      if (currentTr[3].textContent !== "") {
+        currentTr[3].classList.add("activ");
       }
     }
 
@@ -502,7 +504,7 @@ class Game {
   moveRight() {
     if (!this.canContinueGame()) {
       this.getMessage();
-      this.status = 'finished';
+      this.status = "finished";
 
       return;
     }
@@ -554,25 +556,25 @@ class Game {
     for (let i = 0; i < arrayFromTr.length; i++) {
       const currentTr = arrayFromTr[i].children;
 
-      currentTr[0].textContent = arraRow[i][0] === 0 ? '' : arraRow[i][0];
-      currentTr[1].textContent = arraRow[i][1] === 0 ? '' : arraRow[i][1];
-      currentTr[2].textContent = arraRow[i][2] === 0 ? '' : arraRow[i][2];
-      currentTr[3].textContent = arraRow[i][3] === 0 ? '' : arraRow[i][3];
+      currentTr[0].textContent = arraRow[i][0] === 0 ? "" : arraRow[i][0];
+      currentTr[1].textContent = arraRow[i][1] === 0 ? "" : arraRow[i][1];
+      currentTr[2].textContent = arraRow[i][2] === 0 ? "" : arraRow[i][2];
+      currentTr[3].textContent = arraRow[i][3] === 0 ? "" : arraRow[i][3];
 
-      if (currentTr[0].textContent !== '') {
-        currentTr[0].classList.add('activ');
+      if (currentTr[0].textContent !== "") {
+        currentTr[0].classList.add("activ");
       }
 
-      if (currentTr[1].textContent !== '') {
-        currentTr[1].classList.add('activ');
+      if (currentTr[1].textContent !== "") {
+        currentTr[1].classList.add("activ");
       }
 
-      if (currentTr[2].textContent !== '') {
-        currentTr[2].classList.add('activ');
+      if (currentTr[2].textContent !== "") {
+        currentTr[2].classList.add("activ");
       }
 
-      if (currentTr[3].textContent !== '') {
-        currentTr[3].classList.add('activ');
+      if (currentTr[3].textContent !== "") {
+        currentTr[3].classList.add("activ");
       }
     }
 
@@ -643,25 +645,25 @@ class Game {
     for (let i = 0; i < arrayFromTr.length; i++) {
       const currentTr = arrayFromTr[i].children;
 
-      currentTr[0].textContent = arraRow[i][0] === 0 ? '' : arraRow[i][0];
-      currentTr[1].textContent = arraRow[i][1] === 0 ? '' : arraRow[i][1];
-      currentTr[2].textContent = arraRow[i][2] === 0 ? '' : arraRow[i][2];
-      currentTr[3].textContent = arraRow[i][3] === 0 ? '' : arraRow[i][3];
+      currentTr[0].textContent = arraRow[i][0] === 0 ? "" : arraRow[i][0];
+      currentTr[1].textContent = arraRow[i][1] === 0 ? "" : arraRow[i][1];
+      currentTr[2].textContent = arraRow[i][2] === 0 ? "" : arraRow[i][2];
+      currentTr[3].textContent = arraRow[i][3] === 0 ? "" : arraRow[i][3];
 
-      if (currentTr[0].textContent !== '') {
-        currentTr[0].classList.add('activ');
+      if (currentTr[0].textContent !== "") {
+        currentTr[0].classList.add("activ");
       }
 
-      if (currentTr[1].textContent !== '') {
-        currentTr[1].classList.add('activ');
+      if (currentTr[1].textContent !== "") {
+        currentTr[1].classList.add("activ");
       }
 
-      if (currentTr[2].textContent !== '') {
-        currentTr[2].classList.add('activ');
+      if (currentTr[2].textContent !== "") {
+        currentTr[2].classList.add("activ");
       }
 
-      if (currentTr[3].textContent !== '') {
-        currentTr[3].classList.add('activ');
+      if (currentTr[3].textContent !== "") {
+        currentTr[3].classList.add("activ");
       }
     }
 
@@ -687,25 +689,25 @@ class Game {
 
 const game2048 = new Game();
 
-buttonOfStart.addEventListener('click', () => {
+buttonOfStart.addEventListener("click", () => {
   game2048.start();
 });
 
-document.addEventListener('keydown', (ewent) => {
+document.addEventListener("keydown", (ewent) => {
   switch (ewent.key) {
-    case 'ArrowUp':
+    case "ArrowUp":
       game2048.moveUp();
       break;
 
-    case 'ArrowDown':
+    case "ArrowDown":
       game2048.moveDown();
       break;
 
-    case 'ArrowRight':
+    case "ArrowRight":
       game2048.moveRight();
       break;
 
-    case 'ArrowLeft':
+    case "ArrowLeft":
       game2048.moveLeft();
       break;
   }
